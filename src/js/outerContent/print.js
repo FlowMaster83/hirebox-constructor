c7d888abf02c3d511a143b492b3938eccb194d3c
Serhii Kolesnyk <110739720+FlowMaster83@users.noreply.github.com>
2026-01-21 14:56:08 +0200

    upd print.js & print.css

--- src/js/outerContent/print.js
+++ src/js/outerContent/print.js
@@ -1,4 +1,20 @@
 // src/js/outerContent/print.js
+
 export function printResults() {
+  const source = document.querySelector(".modal-results");
+  if (!source) return;
+
+  // создаём print-root
+  const printRoot = document.createElement("div");
+  printRoot.id = "print-root";
+
+  // глубокая копия результата
+  printRoot.appendChild(source.cloneNode(true));
+  document.body.appendChild(printRoot);
+
+  // печать
   window.print();
+
+  // очистка
+  document.body.removeChild(printRoot);
 }
